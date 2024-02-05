3700015326412e44a270e7ea3ece254e59b259c9
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-02-05 16:41:40 +0530

    components, home updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">LUSTFORFOOD</div>
+          <div className="right">
+            <p>Anuradha Cineplex. Chandmari, Guwahati</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Zyan</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/components/WhoWeAre.jsx
+++ frontend/src/components/WhoWeAre.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import {data} from '../restApi.json'
 
 const WhoWeAre = () => {
   return (
-    <div>WhoWeAre</div>
+    <>
+      <section className='who_we_are' id='who_we_are'>
+        <div className="container">
+          <div className="text_banner">
+            {
+              data[0].who_we_are.slice(0,2).map(element=>(
+                <div className="card" key={element.id}>
+                  <h1 className='heading' style={{fontWeight: "100"}}>{element.number}</h1>
+                  <p>{element.title}</p>
+                </div>
+              ))
+            }
+          </div>
+          <div className="image_banner">
+            {/* <img className='gradient_bg' src="center.svg" alt="gradientBg" /> */}
+            <img src="whoweare.png" alt="food" />
+          </div>
+          <div className="text_banner">
+            {
+              data[0].who_we_are.slice(2).map(element=>(
+                <div className="card" key={element.id}>
+                  <h1 className='heading' style={{fontWeight: "100"}}>{element.number}</h1>
+                  <p>{element.title}</p>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        </section> 
+    </>
   )
 }
 
